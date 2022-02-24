fff13339dcc52fb4c3aafaa843ec61327553d713
Jasmine Ly <73720805+JasmineLIE@users.noreply.github.com>
2022-02-23 22:38:31 -0500

    v cre

---
+++ NetBeansProjects/Chapter5_Questions/src/chapter5_questions/Chapter5_Questions.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter5_questions;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hwafu
+ */
+public class Chapter5_Questions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+//        int intNum1;
+//        int intNum2;
+//        
+//        System.out.println("Input a first number:");
+//        intNum1 = sc.nextInt();
+//        
+//        System.out.println("Input a second number:");
+//        intNum2 = sc.nextInt();
+//        
+//        if (intNum1 > intNum2) {
+//            System.out.println("The first number is greater");
+//        }
+//        else if (intNum2 > intNum1){
+//            System.out.println("The second number is greater");
+//        }
+//        else if (intNum1 == intNum2){
+//            System.out.println("The numbers are equal");
+//        }
+//        int intNum;
+//              System.out.println("Put in your score");
+//              intNum = sc.nextInt();
+//              
+//              switch (intNum){
+//                  case 0: System.out.println("Were you even trying>"); break;
+//                  case 1:System.out.println("Better than a 0"); 
+//                  case 2:
+//                  case 3:
+//                  case 4:
+//                  case 5: System.out.println("Almost way there!"); break;
+//                  case 6:
+//                  case 7:
+//                  case 8: System.out.println("Nice job!"); break;
+//                  case 9:
+//                  case 10: System.out.println("Woah, perfect!"); break;
+//                  
+//                      
+//              }
+  
+   
+        // TODO code application logic here
+    }
+
+    private static int nextInt(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
